7f48f3c9534af8ba8dabe0f11a67e4cefa08cdc8
Bartosz Wieczerzak <bartosz.wieczerzak@gmail.com>
2020-01-27 22:40:31 +0100

    initial empty Unity project

---
+++ Assets/Scripts/Owner.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public enum Owner {
+    PLAYER, AI, NONE
+}
---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField]
+    private int units = 0;
+
+    [SerializeField]
+    private Owner owner = Owner.NONE;
+
+    void Update()
+    {
+        
+    }
+
+    public void AddUnits(int amount) {
+        units += amount;
+    }
+
+    public void RemoveUnits(int amount) {
+        units -= amount;
+
+        if (amount < 0) amount = 0;
+    }
+
+    public void ChangeOwnership(Owner newOwner) {
+        owner = newOwner;
+    }
+}
