346da4431c35baeefa88150ee428301b5f04c85a
Bartosz Wieczerzak <bartosz.wieczerzak@gmail.com>
2020-01-30 15:22:18 +0100

    added GameOver mechanics

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    private readonly List<Planet> planets = new List<Planet>();
+
+    void Start() {
+        GameObject[] planetGameObjects = GameObject.FindGameObjectsWithTag(Tag.PLANET);
+
+        foreach (GameObject go in planetGameObjects) {
+            planets.Add(go.GetComponent<Planet>());
+        }
+    }
+
+    void Update() {
+        foreach (Planet planet in planets) {
+            if (planet.Owner == Owner.Player && planet.Units > 0) { 
+                return;
+            }
+
+            GameOver();
+        }
+    }
+
+    private void GameOver() {
+        Debug.Log("Game Over man!");
+    }
+}
