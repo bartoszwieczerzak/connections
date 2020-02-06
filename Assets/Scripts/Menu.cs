5ade76e7e2be9b03e527a9c194f965212cdd2b5c
Kamil Dybicz <kamil@secretescapes.com>
2020-02-07 00:14:16 +0100

    PoC for main menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
+    }
+
+    public void Play()
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quiting the game...");
+
+        Application.Quit();
+    }
+}
