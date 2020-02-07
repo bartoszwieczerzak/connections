9462c949bc6a05003c229c787bc541793bd2bd5d
Kamil Dybicz <kamil@secretescapes.com>
2020-02-07 09:14:14 +0100

    some work around menu, it's crashing for some reason...

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,12 +5,22 @@ public class Menu : MonoBehaviour
 {
     public void MainMenu()
     {
-        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
+        SceneManager.LoadScene(0);
     }
 
     public void Play()
     {
-        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
+        SceneManager.LoadScene(1);
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Quit()
