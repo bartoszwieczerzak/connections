4cd911eadb70e84e37a554fef8b2196da9fd97ef
Kamil Dybicz <kamil@secretescapes.com>
2020-02-13 22:56:01 +0100

    quit button working in Unity Editor too

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -38,6 +38,13 @@ public class Menu : MonoBehaviour
     {
         Debug.Log("Quiting the game...");
 
-        Application.Quit();
+        // save any game data here
+        #if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
